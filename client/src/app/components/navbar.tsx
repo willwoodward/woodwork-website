f0766e7f4c327c242e7edab93706f6bcf9284c1f
willwoodward <will@woodwardweb.com>
2025-05-01 13:30:35 +0100

    styling changes

--- client/src/app/components/navbar.tsx
+++ client/src/app/components/navbar.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4 h-16 text-white fixed top-0 left-0 w-full flex items-center justify-center">
-      <div className="text-lg font-bold absolute left-4">woodwork-engine</div>
-      <ul className="flex space-x-8">
+    <nav className="bg-white dark:bg-gray-900 p-4 h-16 text-black dark:text-white fixed top-0 left-0 w-full flex items-center shadow-sm z-10">
+      <div className="text-lg font-bold px-4 cursor-pointer absolute group">woodwork-engine</div>
+      <ul className="flex space-x-8 mx-auto">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" className="px-3 py-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 hover:text-blue-600 dark:hover:text-blue-100 transition-all">Home</Link>
         </li>
         <li>
-          <Link href="/docs">Docs</Link>
+          <Link href="/docs" className="px-3 py-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 hover:text-blue-600 dark:hover:text-blue-100 transition-all">Docs</Link>
         </li>
       </ul>
     </nav>
